083848cfa6205c3f22df892ca60ba36706f53906
andreprovensi <andreprovensi@gmail.com>
2024-01-11 23:36:22 -0300

    add-rout-and-form

--- app/src/main.py
+++ app/src/main.py
@@ -6,17 +6,21 @@ import os
 # with open('../models/diabetes_model','rb') as file:
 #     classifier = pickle.load(file)
 
-df_columns = [
-    'gender',
-    'age',
-    'hypertension',
-    'heart_disease',
-    'smoking_history',
-    'bmi',
-    'HbA1c_level',
-    'blood_glucose_level',
-    'diabetes'
-]
+df_diabetes = pd.read_csv('../data/diabetes_prediction_dataset.csv',nrows=2)
+
+df_columns = df_diabetes.columns.tolist()
+
+# df_columns = [
+#     'gender',
+#     'age',
+#     'hypertension',
+#     'heart_disease',
+#     'smoking_history',
+#     'bmi',
+#     'HbA1c_level',
+#     'blood_glucose_level',
+#     'diabetes'
+# ]
 
 target = 'diabetes'
 
@@ -36,6 +40,19 @@ def notebook_page():
 def notebook_file():
     return render_template("diabetes_notebook.html")
 
+@app.route('/predict', methods=['POST'])
+def make_prediction():
+    input_data = request.get_json()
+
+    classifier = pickle.load(open('../models/diabetes_model','rb'))
+
+    dict_for_input_dataframe = {feature:[input_data.get(feature)] for feature in df_columns}
+
+    df_input_pipeline = pd.DataFrame.from_dict(dict_for_input_dataframe)
+
+    prediction = classifier.predict_proba()[:,1]
+
+    
 # colunas = None
 # @app.route('/cotacao/',methods=['POST'])
 # def cotacao():
