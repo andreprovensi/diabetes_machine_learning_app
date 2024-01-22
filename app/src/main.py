e888bc7ff596c105bb24e656e99fb71db44e08c3
andreprovensi <andreprovensi@gmail.com>
2024-01-21 21:23:44 -0300

    changed __name__ position

--- app/src/main.py
+++ app/src/main.py
@@ -3,47 +3,48 @@ import pandas as pd
 import pickle
 import os
 
-df_diabetes = pd.read_csv('../data/diabetes_prediction_dataset.csv',nrows=2)
-
-df_columns = df_diabetes.columns.tolist()
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route('/notebook-page')
-def notebook_page():
-    return render_template("notebook.html")
-
-@app.route('/notebook-file')
-def notebook_file():
-    return render_template("diabetes_notebook.html")
-
-@app.route('/predict', methods=['POST'])
-def make_prediction():
-
-    serialized_model = open('../models/diabetes_model','rb')
-    classifier = pickle.load(serialized_model)
-
-    height = float(request.form.get('height'))
-    weight = float( request.form.get('weight'))
-
-    bmi = weight / (height/100)**2
-
-    request_values_dict_for_df = {k:[request.form.get(k)] for k in request.form.to_dict()}
-    request_values_dict_for_df['bmi'] = [bmi]
-
-    df_for_model_prediction = pd.DataFrame.from_dict(request_values_dict_for_df)
-
-    prediction = classifier.predict_proba(df_for_model_prediction)[0][1]
-    print(prediction)
-    
-    serialized_model.close()
-    
-    return jsonify({'prediction':str(round(prediction*100,1))})
-
 if __name__ == '__main__':
+
+    df_diabetes = pd.read_csv('../data/diabetes_prediction_dataset.csv',nrows=2)
+
+    df_columns = df_diabetes.columns.tolist()
+
+    app = Flask(__name__)
+
+
+    @app.route('/')
+    def home():
+        return render_template("index.html")
+
+    @app.route('/notebook-page')
+    def notebook_page():
+        return render_template("notebook.html")
+
+    @app.route('/notebook-file')
+    def notebook_file():
+        return render_template("diabetes_notebook.html")
+
+    @app.route('/predict', methods=['POST'])
+    def make_prediction():
+
+        serialized_model = open('../models/diabetes_model','rb')
+        classifier = pickle.load(serialized_model)
+
+        height = float(request.form.get('height'))
+        weight = float( request.form.get('weight'))
+
+        bmi = weight / (height/100)**2
+
+        request_values_dict_for_df = {k:[request.form.get(k)] for k in request.form.to_dict()}
+        request_values_dict_for_df['bmi'] = [bmi]
+
+        df_for_model_prediction = pd.DataFrame.from_dict(request_values_dict_for_df)
+
+        prediction = classifier.predict_proba(df_for_model_prediction)[0][1]
+        print(prediction)
+        
+        serialized_model.close()
+        
+        return jsonify({'prediction':str(round(prediction*100,1))})
+
     app.run(debug=False)
