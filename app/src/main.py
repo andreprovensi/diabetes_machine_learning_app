ecf79fee960477bf90e74cedecdbab0f31830b00
andreprovensi <andreprovensi@gmail.com>
2024-01-22 21:02:10 -0300

    loading model first

--- app/src/main.py
+++ app/src/main.py
@@ -6,7 +6,17 @@ import os
 def create_API():
 
     app = Flask(__name__)
+    
+    if __name__ == "__main__":
+        path_to_app = '\\'.join(os.getcwd().split('\\')[:-1])
+        model_path = os.path.join(path_to_app,'models','diabetes_model')
+    else:
+        script_dir = os.path.dirname(os.path.abspath('__file__'))
+        model_path = os.path.join(script_dir,'models','diabetes_model')
 
+    serialized_model = open(model_path,"rb")
+
+    classifier = pickle.load(serialized_model)
 
     @app.route('/')
     def home():
@@ -23,30 +33,30 @@ def create_API():
     @app.route('/predict', methods=['POST'])
     def make_prediction():
         
-        if __name__ == "__main__":
-            path_to_app = '\\'.join(os.getcwd().split('\\')[:-1])
-            model_path = os.path.join(path_to_app,'models','diabetes_model')
-        else:
-            script_dir = os.path.dirname(os.path.abspath('__file__'))
-            model_path = os.path.join(script_dir,'models','diabetes_model')
+        # if __name__ == "__main__":
+        #     path_to_app = '\\'.join(os.getcwd().split('\\')[:-1])
+        #     model_path = os.path.join(path_to_app,'models','diabetes_model')
+        # else:
+        #     script_dir = os.path.dirname(os.path.abspath('__file__'))
+        #     model_path = os.path.join(script_dir,'models','diabetes_model')
         
-        with open(model_path,'rb') as serialized_model:
+        # with open(model_path,'rb') as serialized_model:
        
-            classifier = pickle.load(serialized_model)
+        # classifier = pickle.load(serialized_model)
 
-            height = float(request.form.get('height'))
-            weight = float( request.form.get('weight'))
+        height = float(request.form.get('height'))
+        weight = float( request.form.get('weight'))
 
-            bmi = weight / (height/100)**2
+        bmi = weight / (height/100)**2
 
-            request_values_dict_for_df = {k:[request.form.get(k)] for k in request.form.to_dict()}
-            request_values_dict_for_df['bmi'] = [bmi]
+        request_values_dict_for_df = {k:[request.form.get(k)] for k in request.form.to_dict()}
+        request_values_dict_for_df['bmi'] = [bmi]
 
-            df_for_model_prediction = pd.DataFrame.from_dict(request_values_dict_for_df)
+        df_for_model_prediction = pd.DataFrame.from_dict(request_values_dict_for_df)
 
-            prediction = classifier.predict_proba(df_for_model_prediction)[0][1]
-                        
-            return jsonify({'prediction':str(round(prediction*100,1))})
+        prediction = classifier.predict_proba(df_for_model_prediction)[0][1]
+                    
+        return jsonify({'prediction':str(round(prediction*100,1))})
     
     return app
 
